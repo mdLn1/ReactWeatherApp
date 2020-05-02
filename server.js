9709fd8eeea174c6cbd0ad7232f84d9f9971084f
Madalin Preda <madalinc.preda@gmail.com>
2020-05-02 19:13:33 +0100

    edit .env

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ class CustomError extends Error {
     if (statusCode) this.statusCode = statusCode;
   }
 }
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const { check } = require("express-validator");
 const errorChecker = require("./utils/errorCheckingMiddleware");
 const compression = require("compression");
